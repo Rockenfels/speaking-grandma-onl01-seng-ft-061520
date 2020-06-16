0bc94f6dc294b335d8c27eeaa860fceba2262678
Ray Ockenfels <ockenfelsr@gmail.com>
2020-06-16 20:23:49 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@ def speak_to_grandma(phrase)
   test = phrase.upcase
   if phrase == 'I LOVE YOU GRANDMA!'
     return 'I LOVE YOU TOO PUMPKIN!'
-  elsif phrase != test
+  elsif phrase != phrase.upcase
     return 'HUH?! SPEAK UP, SONNY!'
   else 
     return 'NO, NOT SINCE 1938!'
